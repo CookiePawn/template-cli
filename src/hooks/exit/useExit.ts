6c1f651ad5bcf6c6fbc67a3376a7941368dd02f0
CookiePawn <cookiepawn@naver.com>
2025-07-09 10:05:45 +0900

    [new][exit][add exit app hook]

---
+++ src/hooks/exit/useExit.ts
@@ -0,0 +1,32 @@
+import { BackHandler, ToastAndroid } from 'react-native';
+import { useFocusEffect } from '@react-navigation/native';
+import React, { useRef } from 'react';
+import RNExitApp from 'react-native-exit-app';
+
+export const useExit = () => {
+    const backPressedTime = useRef(0);
+
+    useFocusEffect(
+        React.useCallback(() => {
+            const backAction = () => {
+                const currentTime = new Date().getTime();
+                const timeDiff = currentTime - backPressedTime.current;
+
+                if (timeDiff < 2000) { // 2초 이내에 두 번 누른 경우
+                    RNExitApp.exitApp();
+                    return true;
+                }
+
+                backPressedTime.current = currentTime;
+                ToastAndroid.show('한번 더 누르면 종료됩니다', ToastAndroid.SHORT);
+                return true;
+            };
+
+            const backHandler = BackHandler.addEventListener('hardwareBackPress', backAction);
+
+            return () => {
+                backHandler.remove();
+            };
+        }, [])
+    );
+};
