04a039979db80307022d18ee521af808268b411a
CookiePawn <cookiepawn@naver.com>
2025-09-30 15:40:14 +0900

    [ADD][status][add status bar]

--- App.tsx
+++ App.tsx
@@ -3,11 +3,13 @@ import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 import { MainStack } from '@/navigations';
 import { Dialog } from '@/components';
+import { StatusBar } from 'react-native';
 
 const App = () => {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <SafeAreaProvider>
+        <StatusBar barStyle="dark-content" />
         <NavigationContainer>
           <SafeAreaView style={{ flex: 1 }}>
             <Dialog />
